e68cd72017cd02ac0218b6c66d7b2edd804b7347
danielprast <danielprastiwa@gmail.com>
2020-08-07 04:02:41 +0700

    initial commit

---
+++ Solid Code/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Solid Code
+//
+//  Created by Daniel Prastiwa on 07/08/20.
+//  Copyright © 2020 Danielprast. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
